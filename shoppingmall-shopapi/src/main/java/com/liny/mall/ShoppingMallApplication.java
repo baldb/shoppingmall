210b0aa51c31e38bede84d5d433d3d33f9ae81f0
linyi <704600358@qq.com>
2022-08-26 14:15:26 +0800

    基础项目模块的搭建

---
+++ shoppingmall-shopapi/src/main/java/com/liny/mall/ShoppingMallApplication.java
@@ -0,0 +1,17 @@
+package com.liny.mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author linyi
+ * @date 2022/8/26
+ * 1.0
+ */
+
+@SpringBootApplication
+public class ShoppingMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ShoppingMallApplication.class,args);
+    }
+}
